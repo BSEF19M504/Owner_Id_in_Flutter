b532367828de69cf9d28d732c5f398703135ee88
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-12-23 18:51:41 +0500

    Created classes for different information types

---
+++ lib/info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class InfoData{
+  String option;
+  String answer;
+
+  InfoData(this.option, this.answer);
+}
+
+class InfoIcon{
+  IconData icon;
+  String text;
+
+  InfoIcon(this.icon,this.text);
+}
